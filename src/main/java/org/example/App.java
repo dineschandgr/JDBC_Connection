0e3a3a47556fa83cfe8417c914d2bdd5747c3362
Dineshchand Geetha Ravichandran <>
2024-06-05 20:23:43 +0800

    adding files

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -19,8 +20,7 @@ class Employee{
     String position;
     Double Salary;
 
-    public Employee(int id, String name, String position, Double salary) {
-        this.id = id;
+    public Employee(String name, String position, Double salary) {
         this.name = name;
         this.position = position;
         Salary = salary;
@@ -29,15 +29,14 @@ class Employee{
 
 public class App 
 {
-    public static void main( String[] args ) throws SQLException {
+    public static void main( String[] args ) {
+        Employee e1 = new Employee("B", "Intern", 10000.0);
+        Employee e2 = new Employee("C", "Full Time", 20000.0);
 
-        Employee e1 = new Employee(6,"B", "Intern", 10000.0);
-        Employee e2 = new Employee(7,"B", "Intern", 10000.0);
-
-        List<Employee> empList = new ArrayList<>();
-        empList.add(e1);
+        List<Employee> empList = Arrays.asList(e1,e2);
+       /* empList.add(e1);
         empList.add(e2);
-        
+        */
         System.out.println( "Hello World!" );
 
         String url = "jdbc:mysql://localhost:3306/test_new_schema";
@@ -56,14 +55,13 @@ public class App
             Statement stmt = connection.createStatement();
             createTable(stmt);
 
-
             //insert statement
             insertData(stmt, empList);
 
             List<Employee> empListOutput = selectData(stmt);
             System.out.println(empListOutput);
 
-        } catch (SQLException | ClassNotFoundException e) {
+        } catch (Exception e) {
             System.err.println("Connection error: " + e.getMessage());
         }finally{
 
@@ -72,7 +70,7 @@ public class App
     }
 
     public static void createTable(Statement stmt ){
-        String tableSql = "CREATE TABLE IF NOT EXISTS employees"
+        String tableSql = "CREATE TABLE IF NOT EXISTS test_employees"
                 + "(emp_id int PRIMARY KEY AUTO_INCREMENT, name varchar(30),"
                 + "position varchar(30), salary double)";
         try {
@@ -83,29 +81,38 @@ public class App
     }
 
     public static void insertData(Statement stmt, List<Employee> empList){
-        for(Employee e: empList) {
+      /*  for(Employee e: empList) {
             int result = 0;
             try {
                 result = stmt.executeUpdate(
-                        "insert into employees(emp_id,name,position,salary) values('" + e.id + "','" + e.name + "','" + e.position + "'" +
+                        "insert into test_employees(emp_id,name,position,salary) values('x','" + e.name + "','" + e.position + "'" +
                                 ", '" + e.Salary + "' )");
             } catch (SQLException ex) {
                 throw new RuntimeException(ex);
             }
+*/
+            empList.forEach(e -> {
+              int result = 0;
+                        try {
+                            result = stmt.executeUpdate(
+                                    "insert into test_employees(name,position,salary) values('" + e.name + "','" + e.position + "'" +
+                                            ", '" + e.Salary + "' )");
+                        } catch (SQLException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                if (result > 0)
+                    System.out.println("successfully inserted");
+                else
+                    System.out.println( "unsucessful insertion ");
+                    });
 
 
             // if result is greater than 0, it means values
             // has been added
-
-            if (result > 0)
-                System.out.println("successfully inserted");
-            else
-                System.out.println( "unsucessful insertion ");
-        }
     }
 
     public static List<Employee> selectData(Statement stmt){
-        String selectSql = "SELECT * FROM employees";
+        String selectSql = "SELECT * FROM test_employees";
 
         List<Employee> empList1 = new ArrayList<>();
         Employee e = null;
@@ -123,7 +130,7 @@ public class App
                 System.out.print(", Position: " + rs.getString("position"));
                 System.out.println(", salary: " + rs.getDouble("salary"));
 
-                e = new Employee(empId, name, position, salary);
+                e = new Employee( name, position, salary);
                 empList1.add(e);
             }
             // use resultSet here
